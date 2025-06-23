00b7c91b88992ac6bdd6d50414ef25c6b6922045
cuu <gnubsd@me.com>
2025-06-23 15:41:11 +0800

    add sync,default

--- rpi-backlight.c
+++ rpi-backlight.c
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define POWER_FILE "/sys/class/backlight/backlight@0/bl_power"
 #define POWER_ON 0
@@ -22,6 +24,9 @@
 #define BRIGHTNESS_MAX 9
 #define BRIGHTNESS_MIN 1
 #define BRIGHTNESS_STEP 1
+
+#define DEFAULT_CONTENT "3"
+#define FILENAME "rpi-backlight"
 
 int usage(char *argv[]) {
     printf("usage: %s up | down | max | min | on | off\n\n"
@@ -38,6 +43,68 @@ int usage(char *argv[]) {
     return EXIT_FAILURE;
 }
 
+char *get_config_path() {
+    const char *home = getenv("HOME");
+    if (!home) {
+        fprintf(stderr, "Could not find HOME environment variable.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // ~/.config/bat
+    char *path = malloc(strlen(home) + strlen("/.config/") + strlen(FILENAME) + 1);
+    if (!path) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(path, "%s/.config/%s", home, FILENAME);
+    return path;
+}
+
+void ensure_file_exists(const char *filepath) {
+    struct stat st;
+
+    if (stat(filepath, &st) != 0) {
+        // Create the file with default content
+        FILE *f = fopen(filepath, "w");
+        if (!f) {
+            perror("fopen");
+            exit(EXIT_FAILURE);
+        }
+        fputs(DEFAULT_CONTENT, f);
+        fclose(f);
+        printf("Created file with default content: %s\n", filepath);
+    }
+}
+
+
+void store_brightness_in_confg(int value){
+	FILE *brightness_file;
+
+	char *path = get_config_path();
+	ensure_file_exists(path);
+
+    brightness_file = fopen(path, "w");
+    fprintf(brightness_file, "%d", value);
+    fclose(brightness_file); 
+	free(path);
+}
+
+int get_brightness_from_config(){
+	FILE *brightness_file;
+	int brightness_value;
+
+    char *path = get_config_path();
+    ensure_file_exists(path);
+	
+    brightness_file = fopen(path, "r");
+    fscanf(brightness_file, "%d", &brightness_value);
+    fclose(brightness_file);
+	
+	free(path);
+    return brightness_value;
+
+}
 int get_brightness() {
     FILE *brightness_file;
     int brightness_value;
@@ -56,6 +123,9 @@ void set_brightness(int old_bright,int brightness_value) {
    
 
     if ( old_bright == brightness_value ) return;
+	
+	store_brightness_in_confg(brightness_value);
+
     FILE *brightness_file;
     
     brightness_file = fopen(BRIGHTNESS_FILE, "w");
@@ -124,6 +194,12 @@ int main(int argc, char *argv[]) {
     else if (strcmp(argv[1], "min") == 0) {
         brightness_value = BRIGHTNESS_MIN;
     }
+	else if (strcmp(argv[1],"sync") == 0) {
+		brightness_value = get_brightness_from_config();
+		
+	}else if(strcmp(argv[1],"default") == 0){
+		brightness_value = 3;
+	}
     else if (strcmp(argv[1], "on") == 0) {
         power_value = POWER_ON;
     }
